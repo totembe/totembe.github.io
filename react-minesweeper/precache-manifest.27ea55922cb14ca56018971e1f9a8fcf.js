2b388e10f26948000991d785584404f19c4faf1c
totembe <ethembilgin@yahoo.com>
2020-05-06 11:11:51 +0300

    minesweeper

---
+++ react-minesweeper/precache-manifest.27ea55922cb14ca56018971e1f9a8fcf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "651f95599e1c0677efed587a8786a7e5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7007e788dacdf9ffe7d0",
+    "url": "./static/css/main.c25b865e.chunk.css"
+  },
+  {
+    "revision": "e492f4833c2c130360c4",
+    "url": "./static/js/2.2803a5eb.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.2803a5eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7007e788dacdf9ffe7d0",
+    "url": "./static/js/main.0c014654.chunk.js"
+  },
+  {
+    "revision": "c47d1f498ee963a2526b",
+    "url": "./static/js/runtime-main.a4277b7d.js"
+  }
+]);
